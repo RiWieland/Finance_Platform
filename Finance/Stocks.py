1113a5e939b943381fbbdf70c5598d2cdbd38934
RichardWie1and <rich.wieland73@gmail.com>
2020-05-02 12:04:32 +0200

    add Stocks methods

--- Finance/Stocks.py
+++ Finance/Stocks.py
@@ -24,11 +24,32 @@ class Stock:          # Design own class for Stock?
         self.OBV_n = OBV_N
 
         self.stock_df = self.__get_stock_df()
-        
+        self.Dates = self.get_dates()
+        self.returns = self.calc_return()
+
+
     @to_numeric_('stock_frame')
     def __get_stock_df(self):          # use double underscore in beginning here?
 
         self.df_stock = self.stock_frame.loc[self.stock_frame['Symbol'] == self.stock_name]
         self.df_stock.reset_index(inplace=True)
+
+        return self.df_stock
+
+    # Do this with @property
+    def get_dates(self):
+        # Add Month/Year
+        self.df_stock['Year'] = pd.DatetimeIndex(self.df_stock['Trading_Date']).year
+        self.df_stock['Month'] = pd.DatetimeIndex(self.df_stock['Trading_Date']).month
+        self.df_stock['Week'] = pd.DatetimeIndex(self.df_stock['Trading_Date']).week
+        self.df_stock['Day'] = pd.DatetimeIndex(self.df_stock['Trading_Date']).day
+
+        return self.df_stock
+
+    def calc_return(self):
+        self.df_stock['RETURNS'] = self.df_stock['Close'].pct_change()
+        # adjust nan values:
+        self.df_stock['RETURNS'].interpolate('nearest', inplace=True)
+        self.df_stock['RETURNS'].iloc[0] = self.df_stock['RETURNS'].iloc[1]
 
         return self.df_stock
