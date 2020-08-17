f47d901817af63d29deff043c4a26878bc1d1bbd
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-08-17 15:15:22 +0200

    create function Portfolio best return

--- MachineLearning/Labeling.py
+++ MachineLearning/Labeling.py
@@ -23,3 +23,15 @@ def Create_Return_Matrix(Stock_Frame, Period = None):
             
         return return_matrix
 
+def Portfolio_Best_Return(Return_Matrix, Num=3, Period='Month'):
+    Portfolio = dict()
+    temp = dict()
+    for year in Return_Matrix.keys():
+        for month in Return_Matrix[year].keys():
+                 
+            temp[month] = Return_Matrix[year][month].sort_values(ascending=False)[:Num]
+            
+        Portfolio[year] = temp
+        
+    return Portfolio
+
