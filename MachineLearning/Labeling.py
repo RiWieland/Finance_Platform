c4360855d470b187645d07508c7f0efbb0fd5a4f
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-08-17 14:37:30 +0200

    initialize return matrix

--- MachineLearning/Labeling.py
+++ MachineLearning/Labeling.py
@@ -1,22 +1,25 @@
 import pandas as pd
 
 
-def Label_Best_Return(Stock_Frame, Num=3, Period = 'Month'):
+def Create_Return_Matrix(Stock_Frame, Period = None):
         '''
-        Label the Num of Stocks with best preformance among the index per month
+        Create Returns for monthly-base for labeling
         '''
-        portfolio = dict()
-        for _, mon in enumerate(Stock_Frame[Period].unique()):
-            print(mon)
-            df = Stock_Frame.loc[Stock_Frame['Month']==mon]
-            df['RETURNS'] = df['RETURNS'].astype(float)
+        return_matrix = dict()
+        tmp_matrix = dict()
+        if Period is None: # for extending in future for weekly returns
+            year_list = Stock_Frame['Year'].unique()
+            
+            for year in year_list:
+            
+                for _, per in enumerate(Stock_Frame.loc[(Stock_Frame['Year']==year)].Month.unique()):
 
-            portfolio[mon] = df.groupby('Symbol')['RETURNS'].sum().sort_values(ascending=False)[:Num].index
+                    df = Stock_Frame.loc[(Stock_Frame['Year']==year) & (Stock_Frame['Month']==per)]
 
+                    tmp = df.groupby('Symbol')['Adj_Close'].agg(['first', 'last'])
+                    tmp_matrix[per] = tmp['last']- tmp['first']
+                    
+                return_matrix[year] = tmp_matrix
+            
+        return return_matrix
 
-            # Calc returns for each Symbol with group by
-
-            #return_month = df['Close'][-1] - df['Close'][0]
-
-            # find best preforming
-        return portfolio
